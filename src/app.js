45ed31b91568207503dbe6439a2ced48c057f708
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-25 12:30:44 +0330

    feat:Create navbar.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-export const App = () => {};
+import { Navbar } from "./components/navbar";
+import { El } from "./components/shared/el";
+export const App = () => {
+  return El({
+    element: "div",
+    className: "flex flex-col h-screen relative font-Poppins",
+    children: [Navbar()],
+  });
+};
