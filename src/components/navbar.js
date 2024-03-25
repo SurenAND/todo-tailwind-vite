234fd1b166ab62756759f77f88ed2a2cd29b9095
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-25 12:39:38 +0330

    feat:Create nav burger menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,5 +4,25 @@ export const Navbar = () => {
   return El({
     element: "nav",
     className: "bg-purple-800 p-3 flex gap-2 justify-between items-center",
+    children: [
+      El({
+        element: "div",
+        className: "ml-3 text-center flex justify-center gap-2",
+        children: [
+          El({
+            element: "button",
+            className: "cursor-pointer",
+            children: [
+              El({
+                element: "img",
+                className: "w-7",
+                src: "./src/assets/burger-menu.svg",
+                alt: "burger-menu",
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
   });
 };
