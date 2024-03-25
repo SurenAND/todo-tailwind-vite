6b877ce536b165e0bc0a251c2a72564e9c01c41e
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-25 13:25:10 +0330

    feat:Create table head

--- src/components/mainSec.js
+++ src/components/mainSec.js
@@ -9,6 +9,44 @@ export const MainSec = () => {
       El({
         element: "table",
         className: "w-full border-2 overflow-x-auto",
+        children: [
+          El({
+            element: "thead",
+            className: "font-medium",
+            children: [
+              El({
+                element: "tr",
+                children: [
+                  El({
+                    element: "td",
+                    className: "border-l-2 border-b-2 py-4 pl-6",
+                    children: ["Task Name"],
+                  }),
+                  El({
+                    element: "td",
+                    className: "text-center border-l-2 border-b-2",
+                    children: ["Priority"],
+                  }),
+                  El({
+                    element: "td",
+                    className: "text-center border-l-2 border-b-2",
+                    children: ["Status"],
+                  }),
+                  El({
+                    element: "td",
+                    className: "text-center border-l-2 border-b-2",
+                    children: ["Deadline"],
+                  }),
+                  El({
+                    element: "td",
+                    className: "text-center border-l-2 border-b-2",
+                    children: ["Actions"],
+                  }),
+                ],
+              }),
+            ],
+          }),
+        ],
       }),
     ],
   });
