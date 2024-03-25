e4252c5f3dbbf6fce99a5430b7e684e6282cb136
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-25 12:56:42 +0330

    feat:Create nav search

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -29,6 +29,37 @@ export const Navbar = () => {
           }),
         ],
       }),
+      // search , filter & add
+      El({
+        element: "div",
+        className: "flex gap-5 pr-3 sm:justify-center justify-between",
+        children: [
+          // search
+          El({
+            element: "div",
+            className:
+              "flex items-center bg-purple-600 p-2 px-5 gap-1 rounded-md w-full",
+            children: [
+              El({
+                element: "img",
+                className:
+                  "cursor-pointer text-center items-center justify-center mr-2",
+                id: "search",
+                src: "./src/assets/search.svg",
+                alt: "search",
+              }),
+              El({
+                element: "input",
+                className:
+                  "focus:outline-none focus:ring-0 border-none text-white rounded bg-transparent placeholder:text-gray-400 text-sm",
+                type: "text",
+                id: "search-input",
+                placeholder: "Search",
+              }),
+            ],
+          }),
+        ],
+      }),
     ],
   });
 };
