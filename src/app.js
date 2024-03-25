11648391508a1ae2d60ff0f31d9843d7187dab6c
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-25 13:57:16 +0330

    feat:Create addModal.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { addModal } from "./components/addmodal";
 import { MainSec } from "./components/mainSec";
 import { Navbar } from "./components/navbar";
 import { El } from "./components/shared/el";
@@ -5,6 +6,6 @@ export const App = () => {
   return El({
     element: "div",
     className: "flex flex-col h-screen relative font-Poppins",
-    children: [Navbar(), MainSec()],
+    children: [Navbar(), MainSec(), addModal()],
   });
 };
