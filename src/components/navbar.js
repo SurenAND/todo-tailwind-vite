b9322d0e569342a4d6e80feeebc3143cd569e871
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-25 12:38:30 +0330

    feat:Create nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,8 @@
 import { El } from "./shared/el";
 
-export const Navbar = () => {};
+export const Navbar = () => {
+  return El({
+    element: "nav",
+    className: "bg-purple-800 p-3 flex gap-2 justify-between items-center",
+  });
+};
