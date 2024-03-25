c9f2cf6ffe1e2b146b58b14fc2c77950fa065a21
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-25 13:21:50 +0330

    feat:Create table

--- src/components/mainSec.js
+++ src/components/mainSec.js
@@ -4,5 +4,12 @@ export const MainSec = () => {
   return El({
     element: "main",
     className: "h-screen",
+    children: [
+      // table
+      El({
+        element: "table",
+        className: "w-full border-2 overflow-x-auto",
+      }),
+    ],
   });
 };
