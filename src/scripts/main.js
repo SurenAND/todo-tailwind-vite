09f3243b215c01118f7cb0dcd2368b98df03b14b
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-26 15:13:43 +0330

    func:Add addModal to mainjs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,21 @@
+import { closeModal, openModal } from "./modalAction";
+
 // persian date picker
 jalaliDatepicker.startWatch();
+
+// addModal
+const addBtn = document.getElementById("add");
+const modalBox = document.getElementById("modal-box");
+
+// close add modal
+modalBox.addEventListener("click", (e) => {
+  e.target.dataset.close ? closeModal(modalBox) : null;
+});
+// close with cancel btn
+const cancelBtn = document.getElementById("cancel");
+cancelBtn.addEventListener("click", () => closeModal(modalBox));
+
+// open add modal
+addBtn.addEventListener("click", () => {
+  openModal(modalBox);
+});
