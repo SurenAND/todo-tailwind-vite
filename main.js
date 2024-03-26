07b63e627ad2b7ce253284629aa7ed2c10bda4e4
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-26 10:41:34 +0330

    feat:Add persian date picker & main script js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./src/styles/index.css";
 import "./src/persianDatePicker/jalalidatepicker.min.css";
+import "./src/persianDatePicker/jalalidatepicker.min.js";
+import "./src/scripts/main";
 import { App } from "./src/app";
-
 document.querySelector("#app").append(App());
