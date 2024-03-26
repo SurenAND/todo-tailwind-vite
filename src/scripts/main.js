6762fef9b800c2b6352af4204434f5ea367c84cc
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-26 15:14:04 +0330

    func:Add addModal form to mainjs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,11 @@ import { closeModal, openModal } from "./modalAction";
 // persian date picker
 jalaliDatepicker.startWatch();
 
+let tasks = [];
+let isEdit = false;
+let editActive = false;
+let toEdit;
+
 // addModal
 const addBtn = document.getElementById("add");
 const modalBox = document.getElementById("modal-box");
@@ -18,4 +23,19 @@ cancelBtn.addEventListener("click", () => closeModal(modalBox));
 // open add modal
 addBtn.addEventListener("click", () => {
   openModal(modalBox);
+});
+
+// addModal form
+const addForm = document.getElementById("form");
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (isEdit === false) {
+    addToData(e.target);
+  } else {
+    isEdit = false;
+    editActive = true;
+    editRow(e.target, toEdit);
+  }
+
+  closeModal(modalBox);
 });
