bc203483820f5c856c20dc13d8b0caecdb901d6e
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-26 15:39:41 +0330

    func:Add renderTasks to mainjs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -92,3 +92,151 @@ function editRow(e, selectedRow = {}) {
     });
   }
 }
+
+function renderTasks() {
+  const tbody = document.getElementById("tbody");
+  tbody.innerHTML = "";
+
+  if (tasksFromApi) {
+    tasksFromApi.forEach((task) => {
+      const row = document.createElement("tr");
+      row.id = `${task.id}`;
+      row.classList.add("text-center", "border", "h-full", "w-full");
+
+      // taskName column
+      const taskNameCol = document.createElement("td");
+      taskNameCol.classList.add(
+        "text-start",
+        "border-l-2",
+        "border-b-2",
+        "py-4",
+        "pl-6"
+      );
+      taskNameCol.innerText = `${task.taskName}`;
+      row.append(taskNameCol);
+
+      // taskPriority column
+      const taskPriorityCol = document.createElement("td");
+      taskPriorityCol.classList.add("border-l-2", "border-b-2");
+      const prioritySpan = document.createElement("span");
+      prioritySpan.classList.add(
+        "select-none",
+        "rounded-3xl",
+        "border-2",
+        `${handlePriorityBorder(task.taskPriority)}`,
+        `${handlePriorityBg(task.taskPriority)}`,
+        `${handlePriorityText(task.taskPriority)}`,
+        "py-1",
+        "px-3",
+        "font-bold"
+      );
+      prioritySpan.innerText = `${task.taskPriority}`;
+      taskPriorityCol.append(prioritySpan);
+      row.append(taskPriorityCol);
+
+      // taskStatus column
+      const taskStatusCol = document.createElement("td");
+      taskStatusCol.classList.add("border-l-2", "border-b-2");
+      const statusSpan = document.createElement("span");
+      statusSpan.classList.add(
+        "select-none",
+        "rounded-3xl",
+        "border-2",
+        `${handleStatusBorder(task.taskStatus)}`,
+        `${handleStatusBg(task.taskStatus)}`,
+        `${handleStatusText(task.taskStatus)}`,
+        "py-1",
+        "px-3",
+        "font-bold"
+      );
+      statusSpan.innerText = `${task.taskStatus}`;
+      taskStatusCol.append(statusSpan);
+      row.append(taskStatusCol);
+
+      // taskDeadline column
+      const taskDeadlineCol = document.createElement("td");
+      taskDeadlineCol.classList.add("border-l-2", "border-b-2");
+      const deadlineSpan = document.createElement("span");
+      deadlineSpan.classList.add(
+        "select-none",
+        "rounded-3xl",
+        "border-2",
+        "border-blue-400",
+        "py-1",
+        "px-3"
+      );
+      deadlineSpan.innerText = `${task.taskDeadline}`;
+      taskDeadlineCol.append(deadlineSpan);
+      row.append(taskDeadlineCol);
+
+      // taskActions column
+      const taskActionsCol = document.createElement("td");
+      taskActionsCol.classList.add("border-l-2", "border-b-2");
+      const taskActionsSec = document.createElement("div");
+      taskActionsSec.classList.add(
+        "flex",
+        "flex-col",
+        "md:block",
+        "items-center",
+        "justify-center",
+        "p-2",
+        "gap-1"
+      );
+
+      // delete
+      const deleteTask = document.createElement("button");
+      deleteTask.classList.add("bg-red-600", "mx-1", "rounded", "text-center");
+      deleteTask.id = `${task.id}`;
+      const deleteIcon = document.createElement("img");
+      deleteIcon.src = "./assets/delete.svg";
+      deleteIcon.alt = "delete";
+      deleteIcon.classList.add("w-5", "my-1", "mx-2");
+      deleteTask.append(deleteIcon);
+      taskActionsSec.append(deleteTask);
+
+      // edit
+      const editTask = document.createElement("button");
+      editTask.classList.add("bg-blue-600", "mx-1", "rounded", "text-center");
+      editTask.id = `${task.id}`;
+      const editIcon = document.createElement("img");
+      editIcon.src = "./assets/edit.svg";
+      editIcon.alt = "edit";
+      editIcon.classList.add("w-5", "my-1", "mx-2");
+      editTask.append(editIcon);
+      taskActionsSec.append(editTask);
+
+      // view
+      const viewTask = document.createElement("button");
+      viewTask.classList.add("bg-gray-500", "mx-1", "rounded", "text-center");
+      viewTask.id = `${task.id}`;
+      const viewIcon = document.createElement("img");
+      viewIcon.src = "./assets/view.svg";
+      viewIcon.alt = "view";
+      viewIcon.classList.add("w-5", "my-1", "mx-2");
+      viewTask.append(viewIcon);
+      taskActionsSec.append(viewTask);
+
+      // append taskActionsSec
+      taskActionsCol.append(taskActionsSec);
+      row.append(taskActionsCol);
+
+      // append new row
+      tbody.append(row);
+
+      // delete
+      deleteTask.addEventListener("click", (e) => {
+        confirmAndDelete(e, row);
+      });
+
+      // edit
+      editTask.addEventListener("click", (e) => {
+        editRow(e, row);
+      });
+
+      // view
+      viewTask.addEventListener("click", (e) => {
+        viewRow(e, row);
+      });
+    });
+  }
+}
