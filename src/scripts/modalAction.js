397cdea24fd7d36ea0fafd0fbe3226798837bc1e
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-26 15:08:21 +0330

    func: Create modalAction.js

---
+++ src/scripts/modalAction.js
@@ -0,0 +1,15 @@
+import { clearInputs } from "./components";
+
+const addForm = document.getElementById("form");
+// open or close modals
+export function closeModal(item) {
+  clearInputs(addForm);
+  item.classList.add("invisible");
+}
+
+export function openModal(item) {
+  if (isEdit === false) {
+    clearInputs(addForm);
+  }
+  item.classList.remove("invisible");
+}
