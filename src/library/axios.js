362a7b66ea95bbe43663f8ece00a9b565dbd3d1b
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-30 13:46:35 +0330

    func:Update fetchTasks function to handle pagination

--- src/library/axios.js
+++ src/library/axios.js
@@ -3,18 +3,19 @@ import axios from "axios";
 const BASE_URL = "http://localhost:5001";
 
 // Fetch tasks from the API
-export async function fetchTasks() {
-  const response = await axios.get(`${BASE_URL}/tasks`);
-  return response.data;
+export async function fetchTasks(page = 1, perPage = 5, searchParam = "") {
+  const response = await axios.get(
+    `${BASE_URL}/tasks?_page=${page}&_per_page=${perPage}&&taskName_like=${searchParam}`
+  );
+  return {
+    data: response.data,
+    totalPage: Math.ceil(response.headers["x-total-count"] / perPage),
+  };
 }
 
 // Add a task using the API
 export async function addTaskApi(task) {
-  const response = await axios.post(`${BASE_URL}/tasks`, task, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await axios.post(`${BASE_URL}/tasks`, task);
   return response.data;
 }
 
